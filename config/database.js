98f1e7a6f4d1949f98d584324a626d44acd8bbc0
mjknowles <mjknowles23@gmail.com>
2016-06-22 23:10:10 -0500

    Refactoring.

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : 'mongodb://mtduser:mtdpass@ds038739.mlab.com:38739/mean-to-do'
+};
--- server.js
+++ server.js
@@ -7,11 +7,10 @@
     var morgan = require('morgan');             // log requests to the console (express4)
     var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
     var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+    var database = require('./config/database');
 
     // configuration =================
-
-    // public user name and pass meh
-    mongoose.connect('mongodb://mtduser:mtdpass@ds038739.mlab.com:38739/mean-to-do');     // connect to mongoDB database on modulus.io
+    mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
 
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
@@ -20,75 +19,8 @@
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(methodOverride());
 
-    // define model =================
-    var Todo = mongoose.model('Todo', {
-        text : String
-    })
-
-    // routes ======================================================================
-
-    // api ---------------------------------------------------------------------
-    // get all todos
-    app.get('/api/todos', function(req, res) {
-
-        // use mongoose to get all todos in the database
-        Todo.find(function(err, todos) {
-
-            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-            if (err)
-                res.send(err)
-
-            res.json(todos); // return all todos in JSON format
-        });
-    });
-
-    // create todo and send back all todos after creation
-    app.post('/api/todos', function(req, res) {
-        console.log("Post called");
-        // create a todo, information comes from AJAX request from Angular
-        Todo.create({
-            text : req.body.text,
-            done : false
-        }, function(err, todo) {
-            if (err){
-                console.log('there was a mongoose error');
-                res.send(err);
-        }
-            // get and return all the todos after you create another
-            Todo.find(function(err, todos) {
-                if (err)
-                {
-                    console.log(err);
-                    res.send(err)
-                }
-                console.log('post successful');
-                res.json(todos);
-            });
-        });
-
-    });
-
-    // delete a todo
-    app.delete('/api/todos/:todo_id', function(req, res) {
-        Todo.remove({
-            _id : req.params.todo_id
-        }, function(err, todo) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            Todo.find(function(err, todos) {
-                if (err)
-                    res.send(err)
-                res.json(todos);
-            });
-        });
-    });
-
-    // application -------------------------------------------------------------
-    app.get('*', function(req, res) {
-        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-    });
+    // load the routes
+    require('./app/routes')(app);
 
     // listen (start app with node server.js) ======================================
     app.listen(8080);
